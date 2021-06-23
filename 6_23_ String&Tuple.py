4010635d5c02a3aefbe30c60f9131418d5980b4a
programmmer_JJUN <60414838+JANG-hyeonjun@users.noreply.github.com>
2021-06-24 00:40:09 +0900

    Create 6_23_ String&Tuple.py

---
+++ 6_23_ String&Tuple.py
@@ -0,0 +1,33 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    data = 'Hello World'
+    print(data)
+    data = "Don't you know \"Python\"?"
+    print(data)
+
+    a = "Hello"
+    b = "World"
+    print(a + " " + b)
+    a = "String"
+    print(a * 3)
+    a = "ABCDEF"
+    print(a[2:4])
+    a = (1,2,3,4)
+    print(a)
+    #a[2] = 7 
+    #튜플은 한번 선언된 값을 변경불가 
+    #튜플 자료형은 그래프 알고리즘을 구현 할때 자주 사용 
+    #다익스트라 알고리즘은 내부에서 우선순위 큐를 사용하는데 그때 튜플을 사용 
+    
+    
